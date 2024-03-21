37bd4c637b2c65a34f96c4677a437b8c59908c4b
Vishnu v <vishnuvaliyaveetil92@gmail.com>
2024-03-22 03:50:18 +0530

    .env

--- src/Axios/Axios.jsx
+++ src/Axios/Axios.jsx
@@ -1,12 +1,11 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.REACT_APP_BASE_URL,
     // timeout : 15000,
     headers:{
         "Content-Type": "application/json"
         },
-        withCredentials : true
-  
+        withCredentials : import.meta.env.REACT_APP_WITH_CREDENTIALS === 'true'
   });
   export default instance ;
