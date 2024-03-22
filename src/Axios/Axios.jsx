a6ba29dbdfa9ba0d324f8bb20f9e7a32fe3cb6f6
Vishnu v <vishnuvaliyaveetil92@gmail.com>
2024-03-22 10:29:41 +0530

    host commit

--- src/Axios/Axios.jsx
+++ src/Axios/Axios.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: import.meta.env.REACT_APP_BASE_URL,
+    baseURL: 'https://react-project-backend-3.onrender.com',
     // timeout : 15000,
     headers:{
         "Content-Type": "application/json"
         },
-        withCredentials : import.meta.env.REACT_APP_WITH_CREDENTIALS === 'true'
+        withCredentials : true
   });
-  export default instance ;
+  export default instance;
