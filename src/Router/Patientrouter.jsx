7bcf1a88c607c88ec5e5cc57753665520a1c2e7b
Vishnu v <vishnuvaliyaveetil92@gmail.com>
2024-03-27 14:47:14 +0530

    error commit

--- src/Router/Patientrouter.jsx
+++ src/Router/Patientrouter.jsx
@@ -20,37 +20,37 @@ const LazyPrescriptionpage = lazy(() => import('../Userpages/Prescriptionpage'))
 const Patientrouter = () => {
   return (
     <Routes>
-        <Route path='/' element = {<Landingpage />} />
-        <Route path='/signup' element = {<Signup />} />
-        <Route path='/login' element = {<Login />} />
-        <Route path='/otpverification/:number' element = {<Otpverification />} />
-        <Route path='/userforgotpassword' element = {<Forgotpasswordnumber />} />
-        <Route path='/userforgotpasswordconfirm/:userEmail' element = {<Forgotpasswordconfirmation />} />
-        <Route path='/bookappointment' element = {<Bookappointment />} />
-        <Route path='/timeslot/:doctorId' element = {<Slotbookingpage />} />
-        <Route path='/paymentinfo' element = {<Paymentpage />} />
-        <Route path='/paymentsuccess' element = {<Paymentsuccess />} />
-        <Route path='/userprofile' element = {<Userprofilepage />} />
-        <Route path='/edituserprofile/:patientId' element = {<Edituserprofilepage />} />
-        <Route
-  path='/prescriptionview'
-  element={
-    <Suspense
-      fallback={
-        <div style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          minHeight: '100vh'
-        }}>
-          <img src='https://media.architecturaldigest.com/photos/63079fc7b4858efb76814bd2/16:9/w_4000,h_2250,c_limit/9.%20DeLorean-Alpha-5%20%5BDeLorean%5D.jpg' alt='Loading...' />
-        </div>
-      }
-    >
-      <LazyPrescriptionpage />
-    </Suspense>
-  }
-/> 
+      <Route path='/' element={<Landingpage />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/otpverification/:number' element={<Otpverification />} />
+      <Route path='/userforgotpassword' element={<Forgotpasswordnumber />} />
+      <Route path='/userforgotpasswordconfirm/:userEmail' element={<Forgotpasswordconfirmation />} />
+      <Route path='/bookappointment' element={<Bookappointment />} />
+      <Route path='/timeslot/:doctorId' element={<Slotbookingpage />} />
+      <Route path='/paymentinfo' element={<Paymentpage />} />
+      <Route path='/paymentsuccess' element={<Paymentsuccess />} />
+      <Route path='/userprofile' element={<Userprofilepage />} />
+      <Route path='/edituserprofile/:patientId' element={<Edituserprofilepage />} />
+      <Route
+        path='/prescriptionview'
+        element={
+          <Suspense
+            fallback={
+              <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100vh'
+              }}>
+                <img src='https://media.architecturaldigest.com/photos/63079fc7b4858efb76814bd2/16:9/w_4000,h_2250,c_limit/9.%20DeLorean-Alpha-5%20%5BDeLorean%5D.jpg' alt='Loading...' />
+              </div>
+            }
+          >
+            <LazyPrescriptionpage />
+          </Suspense>
+        }
+      />
     </Routes>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps : {
     include : ['jwt-decode']
   },
-  server: {
-    port: 8080
-  },
-  envDir: './'
 })
