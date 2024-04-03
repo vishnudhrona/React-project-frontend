d16b26b54fb42166377af3c0189eb9b7701e62b5
Vishnu v <vishnuvaliyaveetil92@gmail.com>
2024-04-04 04:11:52 +0530

    ok

--- src/Axios/Axios.jsx
+++ src/Axios/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://react-project-backend-6jnf.onrender.com',
+    baseURL: 'https://react-project-backend-bp4k.onrender.com',
     // timeout : 15000,
     headers:{
         "Content-Type": "application/json"
